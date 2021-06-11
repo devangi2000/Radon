2029474ed6c6f9ad563c6de38e5962697c6c3413
devangi2000 <devangidp2000@gmail.com>
2021-06-11 19:15:23 +0530

    changed the template files

--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, 'shop/index.html')
 
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse('Contact Page')
