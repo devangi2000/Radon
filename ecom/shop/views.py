242ac67b4ba4fb831fb298dd00e25e735d3e9933
devangi2000 <devangidp2000@gmail.com>
2021-06-11 17:41:46 +0530

    Added ecom v1

---
+++ ecom/shop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse('About Page')
+
+def contact(request):
+    return HttpResponse('Contact Page')
+
+def tracker(request):
+    return HttpResponse('Tracker Page')
+
+def search(request):
+    return HttpResponse('Search Page')
+
+def productView(request):
+    return HttpResponse('Product View Page')
+
+def checkout(request):
+    return HttpResponse('Checkout Page')
