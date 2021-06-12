8befd3e27113cd260c07597d2b9e6eb5f9d215e7
devangi2000 <devangidp2000@gmail.com>
2021-06-12 19:34:56 +0530

    Added the sliders separately for all products

--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 def index(request):
-    return render(request, 'shop/index.html')
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+    # params = {'no_of_slides': nSlides, 'range': range(1, nSlides),'product': products}
+    # allProds=[[products, range(1, nSlides), nSlides],[products, range(1, nSlides), nSlides]]
+    
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        print(prod)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4)-(n//4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+        print(allProds)
+    params = {'allProds': allProds }
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
